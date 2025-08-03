4accdaea3780d46b16e8b3b7f563f8bda8bae9a6
idriss solhi <93015866+Drisso21@users.noreply.github.com>
2025-08-03 18:10:14 +0200

    new filesddd

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -9,3 +9,8 @@ class BookForm(forms.ModelForm):
 
 class BookSearchForm(forms.Form):
     title = forms.CharField(max_length=100, required=False)
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, label="Name")
+    email = forms.EmailField(label="Email")
+    message = forms.CharField(widget=forms.Textarea, label="Message")
