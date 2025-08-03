c0174f27d23b4b47dfc9b6d329b2806c15814b6b
idriss solhi <93015866+Drisso21@users.noreply.github.com>
2025-08-03 17:33:24 +0200

    new filesddd

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
