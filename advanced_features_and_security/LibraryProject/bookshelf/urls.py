8548c47714a23e9a05c5b66a0e2acde4fd6fb058
idriss solhi <93015866+Drisso21@users.noreply.github.com>
2025-08-03 17:58:23 +0200

    new filesddd

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('add/', views.add_book, name='add_book'),
-    path('', views.book_list, name='book_list'),
+    path('book_list/', views.book_list, name='book_list'),
     path('books/', views.view_books, name='view_books'),
     path('books/create/', views.create_book, name='create_book'),
     path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
